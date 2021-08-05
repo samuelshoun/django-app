1a8871b66fbc58c4dfb073831700ca3a0132bf0e
Samuel Shoun <72581381+samuelshoun@users.noreply.github.com>
2021-08-04 23:14:37 -0500

    fix local env vars

--- django_app/settings.py
+++ django_app/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key)
 # 'vns56su7v^dr%c5(82y&17&8)w6m4six)lmm^cu5k++pqik*w)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.getenv("DEBUG", "False") == "True"
+
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 # ALLOWED_HOSTS = ['147.182.226.17']
